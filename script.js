d7cb3dfc53d92f7f7b17d8e0ecd9f357574aa02b
Benjamin Høj <44369126+Benj1x@users.noreply.github.com>
2023-02-27 13:16:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ console.log("Hello actions");
 //Generate paths for each agent using pathfinding (A* or Dijkstra)
 // OR generate a path 'master' path and have agents path find to the closest point on the master path
 //If an agent is about to collide with another (use either the pythagorean theorem or vector math) generate path around the agent
+
+//Consider using RVO/RVO2 https://gamma.cs.unc.edu/RVO2/
+//Or just detect all agetns around us, and adapt the agent speed to avoid collision
+//or detect all agents infront of us for example FOV 90 degrees, and adapt on what we 'see'
