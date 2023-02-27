d0163f64e1c0d0029ebb6e11469bf756ac3fadc8
Benjamin Høj <44369126+Benj1x@users.noreply.github.com>
2023-02-27 13:07:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Hello actions");
+
+
+//Generate paths for each agent using pathfinding (A* or Dijkstra)
+// OR generate a path 'master' path and have agents path find to the closest point on the master path
+//If an agent is about to collide with another (use either the pythagorean theorem or vector math) generate path around the agent
