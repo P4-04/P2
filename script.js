61fdd7d8169fb6515678a5c14d1bd2e90570f51b
Benjamin Høj <44369126+Benj1x@users.noreply.github.com>
2023-02-27 13:27:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,6 @@ console.log("Hello actions");
 //If an agent is about to collide with another (use either the pythagorean theorem or vector math) generate path around the agent
 
 //Consider using RVO/RVO2 https://gamma.cs.unc.edu/RVO2/
-//Or just detect all agetns around us, and adapt the agent speed to avoid collision
+//Or just detect all agents around us, and adapt the agent speed to avoid collision
 //or detect all agents infront of us for example FOV 90 degrees, and adapt on what we 'see'
+//we can always change the FOV range or other paramteres depending on the agent speed
