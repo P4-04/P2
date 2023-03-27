4179f566c3a0252365a750c13b91119fcbbbf972
Kragemand <ux66at@student.aau.dk>
2023-03-27 10:27:25 +0200

    Fixed issues on dragging walls

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { initCellValues } from './modules/pathfinding.js';
-import { addSpawnArea, getSpawnArea, populate, removeAgentsFromArea } from './modules/agents.js';
+import { addSpawnArea, getSpawnArea, populate, removeAgentsFromArea, anime } from './modules/agents.js';
 import { createGrid, getCellIndex, cellEventHandler, clearCanvas, cellSize, setAddingExit, setAddingSpawn, getAddingExit, getAddingSpawn, endPoint, startPoint, prevExit } from './modules/cells.js';
+
 //Initialize DOM elements
 const closeMenu = document.querySelector("#close");
 const openMenu = document.querySelector("#open");
@@ -157,9 +158,9 @@ startSim.addEventListener("click", function () {
         return;
     }
 
-    initCellValues(cells, endPoint, startPoint);
+    //initCellValues(cells, endPoint, startPoint);
     //AStar
-    populate();
+    //populate();
     anime();
 });
 
@@ -202,7 +203,7 @@ let nextIndex = null;
 let isDragging = false;
 
 drawingArea.addEventListener("mousedown", (event) => {
-    if (getAddingSpawn) {
+    if (getAddingSpawn()) {
         return;
     }
     isDragging = true;
@@ -212,7 +213,7 @@ drawingArea.addEventListener("mousedown", (event) => {
 });
 
 drawingArea.addEventListener("mousemove", (event) => {
-    if (getAddingSpawn) {
+    if (getAddingSpawn()) {
         return;
     }
     if (isDragging == true) {
@@ -225,11 +226,11 @@ drawingArea.addEventListener("mousemove", (event) => {
 });
 
 drawingArea.addEventListener("mouseup", () => {
-    if (getAddingSpawn) {
+    if (getAddingSpawn()) {
         return;
     }
     isDragging = false;
-    setaddingSpawn(false);
+    setAddingSpawn(false);
     prevIndex = null;
     if (menuHidden === false) {
         menu.style.visibility = "visible";
@@ -240,7 +241,7 @@ drawingArea.addEventListener("mouseup", () => {
 let startingCell;
 
 drawingArea.addEventListener("mousedown", (event) => {
-    if (getAddingSpawn) {
+    if (getAddingSpawn()) {
         isDragging = true;
         startingCell = getCellIndex(event.offsetX, event.offsetY);
         prevIndex = getCellIndex(event.offsetX, event.offsetY);
@@ -249,7 +250,7 @@ drawingArea.addEventListener("mousedown", (event) => {
 });
 
 drawingArea.addEventListener("mousemove", (event) => {
-    if (getAddingSpawn && isDragging) {
+    if (getAddingSpawn() && isDragging) {
         let nextIndex = getCellIndex(event.offsetX, event.offsetY);
         if (prevIndex.x != nextIndex.x || prevIndex.y != nextIndex.y) {
             for (let x = nextIndex.x; x >= startingCell.x; --x) {
@@ -264,7 +265,7 @@ drawingArea.addEventListener("mousemove", (event) => {
 });
 
 drawingArea.addEventListener("mouseup", (event) => {
-    if (getAddingSpawn) {
+    if (getAddingSpawn()) {
         isDragging = false;
         setAddingSpawn(false);
         let spawnGroup = [];
