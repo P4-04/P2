aa8c2691592bb1355fd0c21715d78f8ad6333ac6
Benj1x <benjaminh2001@hotmail.dk>
2023-03-30 10:43:22 +0200

    removed more spaghetti

--- modules/pathfinding.js
+++ modules/pathfinding.js
@@ -1,82 +1,7 @@
 import { drawTxt } from './cells.js'
 
-/** 
-* @param {cell} start the start point of our search
-* @param {cell} goal the end point of our search
-* @param {function} h is our heuristic function. h(n) is an estimate of the cost to reach our goal from the node n
-*/
-function AStar(start, goal) {
-    // try
-    // {
-
-        //
-    let openSet = [];
-    openSet.push(start);
-
-    let cameFrom = [];
-
-    let Readcells = []
-
-    let defaults = [1];
-
-    let cScore = defaults.map(x => x * Infinity);
-
-    //for the node n, fScore[n] = cScoren] + h(n). fScore[n] represents our best guess as to how cheap our path would be from start to finish, if it goes through n
-    let fScore = defaults.map(x => x * Infinity);
-
-    //do heurestics
-    //fScore[start] = h(start);
-
-    while (openSet.length > 0) {
-        let winner = 0;
-
-        // This operation can occur in O(Log(N)) time if openSet is a min-heap or a priority queue
-        //This get's the openSet with the lowest value
-
-        let current = openSet[0];
-
-        for (let i = 1; i < openSet.length; i++) {
-            if (openSet[i].f < openSet[winner].f) {
-                winner = i;
-                cameFrom.push(winner);
-                current == i;
-            }
-            if (winner === goal) {
-                return DoPath(cameFrom, winner);
-            }
-            tempset.push(GetNeighbors(openSet[i]), cells);
-            Readcells.push[openSet[i]];
-
-            for (let y = 0; y < Readcells.length; y++) {
-                for (let z = 0; z < tempset.length; z++) {
-                    if (Readcells[y].x === tempset[z].x) {
-                        if (Readcells[y].y === tempset[z].y) {
-                            removeItem(tempset, z);
-                        }
-                    }
-
-                }
-            }
-
-
-            openSet.push(tempset);
-        }
-
-        removeItem(openSet, current);
-
-    }
-
-    // Open set is empty but goal was never reached
-    return false
-    // } catch{e}
-    // {
-    //     let s = "AStar failed :( \n The error is: " + e + " \nI recieved the start point: " + start + " and end point: " + goal;
-    //     console.log(s);
-    //     //sendMessage(s);
-    // }
-}
-
-async function perfMeasure(board, endPoint, startPoint){
+async function perfMeasure(board, endPoint, startPoint)
+{
     const start = performance.now();
 
     initCellValues(board, endPoint, startPoint);
@@ -84,16 +9,16 @@ async function perfMeasure(board, endPoint, startPoint){
     const end = performance.now();
     console.log(`Execution time: ${end - start} ms`);
 }
-
+//If a wall is checked, check how many neighboring walls there are - if theres is only one, this wall is probably then end, therefore all the neighboring walkable cells should have a low value
+//If it has more than one wall as a neighbor, the walkable cells next to, should have a higher value. From this we should also be able to consider if it's the 'middle' of the wall or not.
+//The closer to the middle we get, the futher away the values should increase  
 async function initCellValues(cells, goal, startpoint)
 {
     for (let x = 0; x < cells.length; x++)
     {
         for (let y = 0; y < cells[0].length; y++)
         {
-            cells[x][y].h = heuristic(cells[x][y], goal, cells);
-            
-            cells[x][y].vh = visualDistance(cells[x][y], goal);
+            cells[x][y].h = heuristic(cells[x][y], goal, cells);            
             cells[x][y].f = cells[x][y].g + cells[x][y].h;
             //cells[x][y].f = Math.max((cells[x][y].g - Math.min(0))/Math.max(10)-Math.min(0)*10)
             /* Uncomment if you need to see the value of the cells*/
@@ -113,45 +38,32 @@ function SetEssenVariables(Width, Height, Size)
     pCellSize = Size;
 }
 
-function removeItem(arr, value) {
-    let index = arr.indexOf(value);
-    if (index > -1) {
-      arr.splice(index, 1);
-    }
-    return arr;
-  }
-
 function GetNeighbors(cell, cells)
 {
     let neighbors = [];
     //Get x neighbors
     if (cell.x != 0)
     {
-        //neighbors[0] = cells[(cell.x/pCellSize)-1][cell.y/pCellSize];
-        //console.log("Current cell: " + cell.x + " " + cell.y);
-        //console.log(neighbors[0].x + " " + neighbors[0].y);
-    } else{
-        console.log("Current cell: " + cell.x + " " + cell.y);
-        console.log("false");
+        neighbors[0] = cells[(cell.x/pCellSize)-1][cell.y/pCellSize];
+            
+    } 
+    else{
+        
     }
 
     if (cell.x != cells[cells.length-1][cells[0].length-1].x)
     {
-        
         neighbors[1] = cells[(cell.x/pCellSize)+1][cell.y/pCellSize];
-        }
+    }
+    
         //Get y neighbors
     if (cell.y != 0){
-        //console.log(cells[cell.x][cell.y-1])
-        //neighbors[2] = cells[cell.x/pCellSize][(cell.y/pCellSize)-1];
-        //console.log(neighbors[2].x + " " + neighbors[2].y);
+        neighbors[2] = cells[cell.x/pCellSize][(cell.y/pCellSize)-1];
     }
 
     if (cell.y != cells[0][cells[0].length-1].y)
     {
-        //console.log(cells[cell.x][cell.y+1])
         neighbors[3] = cells[cell.x/pCellSize][(cell.y/pCellSize)+1];
-
     }
 
     //Visualisation of our astar scan
@@ -187,67 +99,7 @@ function heuristic(cell, goal, cells)
         
     }
 }
-/**Uses the manhatten distance formula to decide on a visual distance
- * @param {cell} NCell the neighbor cell
- * @param {cell} goal the place to reach
-*/
-function visualDistance(NCell, goal)
-{
-    return Math.abs(start.x - goal.x) + Math.abs(start.y - goal.y);
-}
 
-
-/*
-function reconstruct_path(cameFrom, current)
-    total_path := {current}
-    while current in cameFrom.Keys:
-        current := cameFrom[current]
-        total_path.prepend(current)
-    return total_path
-
-// A* finds a path from start to goal.
-// h is the heuristic function. h(n) estimates the cost to reach goal from node n.
-function A_Star(start, goal, h)
-    // The set of discovered nodes that may need to be (re-)expanded.
-    // Initially, only the start node is known.
-    // This is usually implemented as a min-heap or priority queue rather than a hash-set.
-    openSet := {start}
-
-    // For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from the start
-    // to n currently known.
-    cameFrom := an empty map
-
-    // For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
-    gScore := map with default value of Infinity
-    gScore[start] := 0
-
-    // For node n, fScore[n] := gScore[n] + h(n). fScore[n] represents our current best guess as to
-    // how cheap a path could be from start to finish if it goes through n.
-    fScore := map with default value of Infinity
-    fScore[start] := h(start)
-
-    while openSet is not empty
-        // This operation can occur in O(Log(N)) time if openSet is a min-heap or a priority queue
-        current := the node in openSet having the lowest fScore[] value
-        if current = goal
-            return reconstruct_path(cameFrom, current)
-
-        openSet.Remove(current)
-        for each neighbor of current
-            // d(current,neighbor) is the weight of the edge from current to neighbor
-            // tentative_gScore is the distance from start to the neighbor through current
-            tentative_gScore := gScore[current] + d(current, neighbor)
-            if tentative_gScore < gScore[neighbor]
-                // This path to neighbor is better than any previous one. Record it!
-                cameFrom[neighbor] := current
-                gScore[neighbor] := tentative_gScore
-                fScore[neighbor] := tentative_gScore + h(neighbor)
-                if neighbor not in openSet
-                    openSet.add(neighbor)
-
-    // Open set is empty but goal was never reached
-    return failure
-*/
 function sendMessage(error) {
     const request = new XMLHttpRequest();
     request.open("POST", "https://discord.com/api/webhooks/1086218483404124190/3vs52JSZdB5vwQF2GsGSn5aT6VLDXdCiDYUCGe252Gn3gTFr5dC_w7NLeXC8rdu10bpB");
