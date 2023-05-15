4c226ebcd4d793126abb065e49e8def1f691523e
Emil <global52600894@gmail.com>
2023-05-15 18:01:40 +0200

    description for unit testing

--- tests/agents.test.js
+++ tests/agents.test.js
@@ -9,28 +9,34 @@ describe('populate', () => {
         populate(spawnAreas, cellSize, numAgents);
       }).toThrowError('Please add spawn areas');
     });
+
+    //add more tests here :D
   });
   
-  describe('checkAgentDistance', () => {
-    test('returns false when distance is less than minAgentDistance + agent.fattiness * 2', () => {
-      const x = 10;
-      const y = 20;
-      const minAgentDistance = 5;
-      const agents = [
-        { x: 15, y: 25, fattiness: 2 },
-        { x: 30, y: 35, fattiness: 3 },
-      ];
-      const result = checkAgentDistance(x, y, minAgentDistance, agents);
-      expect(result).toBe(false);
-    });
-  
-    test('returns true when there are no agents in the array', () => {
-      const x = 10;
-      const y = 20;
-      const minAgentDistance = 5;
-      const agents = [];
-      const result = checkAgentDistance(x, y, minAgentDistance, agents);
-      expect(result).toBe(true);
-    });
+describe('checkAgentDistance', () => {
+  test('returns false when distance is less than minAgentDistance + agent.fattiness * 2', () => {
+    const x = 10;
+    const y = 20;
+    const minAgentDistance = 5;
+    const agents = [
+      { x: 15, y: 25, fattiness: 2 },
+      { x: 30, y: 35, fattiness: 3 },
+    ];
+    const result = checkAgentDistance(x, y, minAgentDistance, agents);
+    expect(result).toBe(false);
   });
+
+  test('returns true when there are no agents in the array', () => {
+    const x = 10;
+    const y = 20;
+    const minAgentDistance = 5;
+    const agents = [];
+    const result = checkAgentDistance(x, y, minAgentDistance, agents);
+    expect(result).toBe(true);
+  });
+
+  //add more tests here :D
+});
+
+//follow the same structure as above for tests on other functions
   
