9a41ccecbd0c5747e030de6c083231c3d163ffb9
Benj1x <benjaminh2001@hotmail.dk>
2023-05-24 15:51:52 +0200

    Updated agent weight to match main

--- modules/agents.js
+++ modules/agents.js
@@ -401,7 +401,7 @@ function animate(start) {
 
             //Applying agent weight to cell
             //Makes more densely packed cells less desirable
-            let agentWeight = 1.5;
+            let agentWeight = 1.25;
             if (!agents[i].prevCell2.x) {
                 agents[i].prevCell2.x = Math.floor(newX / cellSize);
                 agents[i].prevCell2.y = Math.floor(newY / cellSize);
